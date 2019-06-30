f5060d05a5f5d19c3774e5b554131d948d413a30
Ushir Raval <ushir.raval@gmail.com>
2019-06-30 15:30:15 +0200

    Update SingleMatsimRun.java

--- src/main/java/org/matsim/ConcurrentMatsimRuns/SingleMatsimRun.java
+++ src/main/java/org/matsim/ConcurrentMatsimRuns/SingleMatsimRun.java
@@ -7,7 +7,6 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
 
-//public class SingleMatsimRun {
 public class SingleMatsimRun extends Thread {
 
     String[] args;
